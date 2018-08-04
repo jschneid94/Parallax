294e5dfff30a3fc7c4193a1d8ba877ebb48b6cf3
SchneiderMan <jschneid94@gmail.com>
2018-08-03 23:20:01 -0700

    Added a few more sections and enhanced styling.

--- assets/javascript/_main.js
+++ assets/javascript/_main.js
@@ -1,3 +1,26 @@
+// Back to top button appears when scrolling down the page
+if ($('#back-to-top').length) {
+    var scrollTrigger = 200,
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
+
 $(document).ready(function(){
     $('.tabs').tabs();
-  });
+});
