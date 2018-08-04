7396d3a401d0d9fc5bb9815a44c528fd2742a418
SchneiderMan <jschneid94@gmail.com>
2018-08-03 20:54:00 -0700

    Added a banner and project section.

--- assets/javascript/_main.js
+++ assets/javascript/_main.js
@@ -1,9 +1,3 @@
-( function( $ ) {
-    // Init Skrollr
-    var s = skrollr.init({
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            //console.log(data.curTop);
-        }
-    });
-} )( jQuery );
+$(document).ready(function(){
+    $('.tabs').tabs();
+  });
